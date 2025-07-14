3c006afa1b3d078f977a35cf9a45af889ec6c582
shawn2400 <124217096+shawn2400@users.noreply.github.com>
2025-07-14 11:12:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import json
+import os
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "🔧 Flask proxy is running!"
+
+@app.route('/live')
+def live():
+    try:
+        with open("live.json", "r") as f:
+            data = json.load(f)
+        return jsonify(data)
+    except FileNotFoundError:
+        return "live.json not found", 404
+
+if _name_ == '_main_':
+    print("🔗 Proxy is running...")
+    app.run(host='0.0.0.0', port=5000)
